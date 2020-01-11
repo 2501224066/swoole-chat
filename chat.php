e9e3007f49e29cc05133f21e3d1136570562e78d
zj <2501224066@qq.com>
2020-01-11 16:58:33 +0800

    style: 添加字体，调整样式

--- chat.php
+++ chat.php
@@ -6,8 +6,8 @@ $ws = new swoole_websocket_server("0.0.0.0", 1000);
 //监听WebSocket连接打开事件
 $ws->on('open', function ($ws, $request) {
     $ws->push($request->fd, json_encode([
-        "name" => "游客" . ($request->fd + 10000),
-        "content" => "连接成功.",
+        "name" => "Tourist[" . ($request->fd + 10000) . "]",
+        "content" => "Successful connection...",
         "time" => date('Y-m-d H:i:s')
     ]));
 });
@@ -17,7 +17,7 @@ $ws->on('message', function ($ws, $frame) {
     $pushId = $frame->fd;
     foreach ($ws->connections as $fd) {
         $ws->push($fd, json_encode([
-            "name" => "游客" . ($pushId + 10000),
+            "name" => "Tourist[" . ($pushId + 10000) . ']',
             "content" => $frame->data,
             "time" => date('Y-m-d H:i:s')
         ]));
